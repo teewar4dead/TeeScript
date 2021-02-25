50b50d34ac9e176fc3d02e519cbf42d5c14bf844
teewar4dead <79666832+teewar4dead@users.noreply.github.com>
2021-02-26 02:38:14 +0600

    Add files via upload

---
+++ Tee.LoneDruid/Bear/Items/item_bloodthorn.cs
@@ -0,0 +1,21 @@
+﻿using Divine;
+using Divine.SDK.Extensions;
+using System;
+
+namespace TeeLoneDruid.Bear.Items
+{
+    class item_bloodthorn
+    {
+        public item_bloodthorn(Unit unit, Unit target)
+        {
+            var item = AbilityId.item_bloodthorn;
+
+
+
+            if (BearConfig.AbilityToggler.GetValue(item) && itemsHelper.FindItem(unit, item) && unit.Position.Distance2D(target.Position) <= 350)
+            {
+                itemsHelper.CastItemTarget(unit, target, item);
+            }
+        }
+    }
+}
