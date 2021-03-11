3f91f2e5841019a070c96dc7d6bef5ffae1803e0
teewar4dead <79666832+teewar4dead@users.noreply.github.com>
2021-03-12 02:02:57 +0600

    Add files via upload

---
+++ Tee.Snatcher/GlobalMenu.cs
@@ -0,0 +1,16 @@
+﻿using Divine.Menu;
+using Divine.Menu.Items;
+
+namespace Tee.Snatcher
+{
+    internal class GlobalMenu
+    {
+        public static MenuSwitcher OnOff { get; set; }
+        public GlobalMenu()
+        {
+            var RootMenu = MenuManager.CreateRootMenu("Tee.Snatcher");
+            OnOff = RootMenu.CreateSwitcher("Enable", true);
+            new Snatcher();
+        }
+    }
+}
---
+++ Tee.Snatcher/Snatcher.cs
@@ -0,0 +1,40 @@
+﻿using Divine;
+using Divine.SDK.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tee.Snatcher
+{
+    class Snatcher
+    {
+        public static readonly Hero MyHero = EntityManager.LocalHero;
+        public Snatcher()
+        {
+            GlobalMenu.OnOff.ValueChanged += OnOff_ValueChanged;
+        }
+
+        private void OnOff_ValueChanged(Divine.Menu.Items.MenuSwitcher switcher, Divine.Menu.EventArgs.SwitcherEventArgs e)
+        {
+            if (e.Value)
+            {
+                UpdateManager.IngameUpdate += UpdateManager_IngameUpdate;
+            }
+            else
+            {
+                UpdateManager.IngameUpdate -= UpdateManager_IngameUpdate;
+            }
+        }
+
+        private void UpdateManager_IngameUpdate()
+        {
+            var Rune = EntityManager.GetEntities<Rune>().FirstOrDefault(x => x.Distance2D(MyHero.Position) <= 300 && (x.RuneType & RuneType.Bounty) == RuneType.Bounty && x.IsVisible && x.IsValid);
+           if(Rune != null)
+            {
+                MyHero.PickUp(Rune);
+            }
+        }
+    }
+}
