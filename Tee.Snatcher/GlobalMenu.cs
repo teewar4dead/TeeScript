b5ee4b20dc8f8d4efc5ddf2b9dd3765f3f4b1104
teewar4dead <79666832+teewar4dead@users.noreply.github.com>
2021-03-14 17:33:46 +0600

    -Add Rune -Add Aegis -Add Cheese -Add Refresh -Add Aghanim -Add Gem -Add Rapier -Add Neutrals

--- Tee.Snatcher/GlobalMenu.cs
+++ Tee.Snatcher/GlobalMenu.cs
@@ -1,15 +1,46 @@
-﻿using Divine.Menu;
+﻿using Divine;
+using Divine.Menu;
 using Divine.Menu.Items;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Tee.Snatcher
 {
     internal class GlobalMenu
     {
+        public static Dictionary<string, bool> ListRuneToggler = new Dictionary<string, bool>
+            {
+                { "Bounty", true },
+                { "Aegis", true },
+                { "Cheese", true },
+                { "Refresher", true },
+                { "Aghanim", true },
+                { "Gem", true },
+                { "Rapier", true },
+                { "Neutrals", true }
+            };
         public static MenuSwitcher OnOff { get; set; }
+        public static MenuToggler PicUp { get; set; }
         public GlobalMenu()
         {
+            var testImage = Assembly.GetExecutingAssembly().GetManifestResourceStream(@"Tee.Snatcher.Bounty_Rune_minimap_icon.png");
+
+            RendererManager.LoadTexture("Bounty", testImage);
+            RendererManager.LoadTexture("Aegis", "panorama/images/items/aegis_png.vtex_c");
+            RendererManager.LoadTexture("Cheese", "panorama/images/items/cheese_png.vtex_c");
+            RendererManager.LoadTexture("Refresher", "panorama/images/items/refresher_shard_png.vtex_c");
+            RendererManager.LoadTexture("Aghanim", "panorama/images/compendium/international2020/aghanim_icon_psd.vtex_c");
+            RendererManager.LoadTexture("Gem", "panorama/images/items/gem_png.vtex_c");
+            RendererManager.LoadTexture("Rapier", "panorama/images/items/rapier_png.vtex_c");
+            RendererManager.LoadTexture("Neutrals", "panorama/images/items/poor_mans_shield_png.vtex_c");
+
+
+
+
             var RootMenu = MenuManager.CreateRootMenu("Tee.Snatcher");
             OnOff = RootMenu.CreateSwitcher("Enable", true);
+
+            PicUp = RootMenu.CreateToggler("PickUp", ListRuneToggler);
             new Snatcher();
         }
     }
--- Tee.Snatcher/Snatcher.cs
+++ Tee.Snatcher/Snatcher.cs
@@ -1,5 +1,6 @@
 ﻿using Divine;
 using Divine.SDK.Extensions;
+using Divine.SDK.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,24 @@ namespace Tee.Snatcher
 {
     class Snatcher
     {
+
+        private readonly Sleeper Sleeper1 = new Sleeper();
+        private readonly Sleeper Sleeper2 = new Sleeper();
+        private readonly Sleeper Sleeper3 = new Sleeper();
+        private readonly Sleeper Sleeper4 = new Sleeper();
+        private readonly Sleeper Sleeper5 = new Sleeper();
+        private readonly Sleeper Sleeper6 = new Sleeper();
+        private readonly Sleeper Sleeper7 = new Sleeper();
+
+
+        private readonly Sleeper Sleeper11 = new Sleeper();
+        private readonly Sleeper Sleeper22 = new Sleeper();
+        private readonly Sleeper Sleeper33 = new Sleeper();
+        private readonly Sleeper Sleeper44 = new Sleeper();
+        private readonly Sleeper Sleeper55 = new Sleeper();
+        private readonly Sleeper Sleeper66 = new Sleeper();
+        private readonly Sleeper Sleeper77 = new Sleeper();
+
         public static readonly Hero MyHero = EntityManager.LocalHero;
         public Snatcher()
         {
@@ -30,11 +49,161 @@ namespace Tee.Snatcher
 
         private void UpdateManager_IngameUpdate()
         {
-            var Rune = EntityManager.GetEntities<Rune>().FirstOrDefault(x => x.Distance2D(MyHero.Position) <= 300 && (x.RuneType & RuneType.Bounty) == RuneType.Bounty && x.IsVisible && x.IsValid);
-           if(Rune != null)
+            var Bounty = EntityManager.GetEntities<Rune>().FirstOrDefault(x => x.Distance2D(MyHero.Position) <= 300 && (x.RuneType & RuneType.Bounty) == RuneType.Bounty && x.IsVisible && x.IsValid);
+            var Aegis = EntityManager.GetEntities<PhysicalItem>().FirstOrDefault(x => x.Distance2D(MyHero.Position) <= 300 && x.Item.Id == AbilityId.item_aegis && x.IsVisible && x.IsValid);
+            var Cheese = EntityManager.GetEntities<PhysicalItem>().FirstOrDefault(x => x.Distance2D(MyHero.Position) <= 300 && x.Item.Id == AbilityId.item_cheese && x.IsVisible && x.IsValid); ;
+            var Refresher = EntityManager.GetEntities<PhysicalItem>().FirstOrDefault(x => x.Distance2D(MyHero.Position) <= 300 && x.Item.Id == AbilityId.item_refresher_shard && x.IsVisible && x.IsValid);
+            var Aghanim = EntityManager.GetEntities<PhysicalItem>().FirstOrDefault(x => x.Distance2D(MyHero.Position) <= 300 && x.Item.Id == AbilityId.item_ultimate_scepter_2 && x.IsVisible && x.IsValid);
+            var Gem = EntityManager.GetEntities<PhysicalItem>().FirstOrDefault(x => x.Distance2D(MyHero.Position) <= 300 && x.Item.Id == AbilityId.item_gem && x.IsVisible && x.IsValid);
+            var Rapier = EntityManager.GetEntities<PhysicalItem>().FirstOrDefault(x => x.Distance2D(MyHero.Position) <= 300 && x.Item.Id == AbilityId.item_rapier && x.IsVisible && x.IsValid);
+            var Neutrals = EntityManager.GetEntities<PhysicalItem>().FirstOrDefault(x => x.Distance2D(MyHero.Position) <= 300 && x.Item.KeyValue.GetKeyValue("ItemIsNeutralDrop").GetBooleon() && x.IsVisible && x.IsValid);
+
+
+
+            try
             {
-                MyHero.PickUp(Rune);
+
             }
+            catch (Exception)
+            {
+
+            }
+
+
+
+
+            if (Bounty != null && GlobalMenu.ListRuneToggler["Bounty"])
+            {
+                MyHero.PickUp(Bounty);
+            }
+
+
+
+            if (Aegis != null && GlobalMenu.ListRuneToggler["Aegis"] && (MyHero.Inventory.FreeMainSlots.Count() != 0) && MyHero.ActiveShop != ShopType.Base && !Sleeper11.Sleeping)
+            {
+                MyHero.PickUp(Aegis);
+                Sleeper11.Sleep(150);
+            }
+            else if (Aegis != null && GlobalMenu.ListRuneToggler["Aegis"] && MyHero.Inventory.FreeBackpackSlots.Count() != 0 && MyHero.ActiveShop != ShopType.Base && !Sleeper1.Sleeping)
+            {
+                Console.WriteLine("Yes");
+                MyHero.Inventory.Move(MyHero.Inventory.MainItems.OrderBy(x => x.Cost).FirstOrDefault(x=> x.IsSellable), MyHero.Inventory.FreeBackpackSlots.FirstOrDefault());
+                Sleeper1.Sleep(1000);
+
+            }
+            else if (Aegis != null && GlobalMenu.ListRuneToggler["Aegis"] && MyHero.Inventory.GetFreeSlots(ItemSlot.BackPack_1, ItemSlot.MainSlot_6).Count() == 0 && MyHero.ActiveShop != ShopType.Base && !Sleeper1.Sleeping)
+            {
+                var item = MyHero.Inventory.Items.OrderBy(x => x.Cost).FirstOrDefault(x => x.Id != AbilityId.item_tpscroll && x.Cost < 449);
+                if (item != null)
+                {
+                    Console.WriteLine("1");
+                    MyHero.Drop(item, MyHero.Position);
+                }
+            }
+
+
+
+
+            if (Cheese != null && GlobalMenu.ListRuneToggler["Cheese"] && MyHero.Inventory.GetFreeSlots(ItemSlot.BackPack_1, ItemSlot.MainSlot_6).Count() != 0 && MyHero.ActiveShop != ShopType.Base && !Sleeper22.Sleeping)
+            {
+
+                MyHero.PickUp(Cheese);
+                Sleeper22.Sleep(150);
+                Console.WriteLine("2");
+            }
+
+
+
+
+
+            if (Refresher != null && GlobalMenu.ListRuneToggler["Refresher"] && MyHero.Inventory.GetFreeSlots(ItemSlot.BackPack_1, ItemSlot.MainSlot_6).Count() != 0 && MyHero.ActiveShop != ShopType.Base && !Sleeper33.Sleeping)
+            {
+                MyHero.PickUp(Refresher);
+                Sleeper33.Sleep(150);
+                Console.WriteLine("3");
+            }
+
+
+
+
+            if (Aghanim != null && GlobalMenu.ListRuneToggler["Aghanim"] && (MyHero.Inventory.FreeMainSlots.Count() != 0) && MyHero.ActiveShop != ShopType.Base && !Sleeper44.Sleeping)
+            {
+                MyHero.PickUp(Aghanim);
+                Sleeper44.Sleep(150);
+                Console.WriteLine("4");
+            }
+            else if (Aghanim != null && GlobalMenu.ListRuneToggler["Aghanim"] && MyHero.Inventory.FreeBackpackSlots.Count() != 0 && MyHero.ActiveShop != ShopType.Base && !Sleeper2.Sleeping)
+            {
+                MyHero.Inventory.Move(MyHero.Inventory.MainItems.OrderBy(x => x.Cost).FirstOrDefault(x => x.IsSellable), MyHero.Inventory.GetFreeSlots(ItemSlot.BackPack_1, ItemSlot.BackPack_3).FirstOrDefault());
+                Sleeper2.Sleep(1000);
+                Console.WriteLine("5");
+            }
+
+
+
+
+            if (Gem != null && GlobalMenu.ListRuneToggler["Gem"] && (MyHero.Inventory.FreeMainSlots.Count() != 0) && MyHero.ActiveShop != ShopType.Base && !Sleeper55.Sleeping)
+            {
+                Console.WriteLine("6");
+                MyHero.PickUp(Gem);
+                Sleeper55.Sleep(150);
+            }
+            else if (Gem != null && GlobalMenu.ListRuneToggler["Gem"] && MyHero.Inventory.FreeBackpackSlots.Count() != 0 && MyHero.ActiveShop != ShopType.Base && !Sleeper3.Sleeping)
+            {
+                Console.WriteLine("7");
+                MyHero.Inventory.Move(MyHero.Inventory.MainItems.OrderBy(x => x.Cost).FirstOrDefault(x => x.IsSellable), MyHero.Inventory.GetFreeSlots(ItemSlot.BackPack_1, ItemSlot.BackPack_3).FirstOrDefault());
+                Sleeper3.Sleep(1000);
+            }
+
+
+
+            Console.WriteLine(MyHero.Inventory.FreeMainSlots.Count());
+
+            if (Rapier != null && GlobalMenu.ListRuneToggler["Rapier"] && (MyHero.Inventory.FreeMainSlots.Count() != 0) && MyHero.ActiveShop != ShopType.Base && !Sleeper66.Sleeping)
+            {
+                Console.WriteLine("8");
+                MyHero.PickUp(Rapier);
+                Sleeper66.Sleep(150);
+            }
+            else if (Rapier != null && GlobalMenu.ListRuneToggler["Rapier"] && MyHero.Inventory.FreeBackpackSlots.Count() != 0 && MyHero.ActiveShop != ShopType.Base && !Sleeper4.Sleeping)
+            {
+
+                Console.WriteLine("9");
+                MyHero.Inventory.Move(MyHero.Inventory.MainItems.OrderBy(x => x.Cost).FirstOrDefault(x => x.IsSellable), MyHero.Inventory.GetFreeSlots(ItemSlot.BackPack_1, ItemSlot.BackPack_3).FirstOrDefault());
+                Sleeper4.Sleep(1000);
+            }
+            else if (Rapier != null && GlobalMenu.ListRuneToggler["Rapier"] && MyHero.Inventory.GetFreeSlots(ItemSlot.BackPack_1, ItemSlot.MainSlot_6).Count() == 0 && MyHero.ActiveShop != ShopType.Base && !Sleeper1.Sleeping)
+            {
+                var item = MyHero.Inventory.Items.OrderBy(x => x.Cost).FirstOrDefault(x => x.Id != AbilityId.item_tpscroll && x.Cost < 449);
+                if (item != null)
+                {
+                    MyHero.Drop(item, MyHero.Position);
+                    Sleeper1.Sleep(1000);
+                }
+            }
+
+
+            if (Neutrals != null && GlobalMenu.ListRuneToggler["Neutrals"] && (MyHero.Inventory.FreeBackpackSlots.Count() != 0 || MyHero.Inventory.NeutralItem == null) && MyHero.ActiveShop != ShopType.Base && !Sleeper77.Sleeping)
+            {
+
+                MyHero.PickUp(Neutrals);
+                Console.WriteLine(Neutrals?.Name); //
+                Sleeper77.Sleep(150);
+            }
+            else if (Neutrals != null && GlobalMenu.ListRuneToggler["Neutrals"] && MyHero.Inventory.FreeMainSlots.Count() != 0 && !Sleeper5.Sleeping && MyHero.ActiveShop != ShopType.Base)
+            {
+                
+                if (MyHero.Inventory.Move(MyHero.Inventory.BackpackItems.FirstOrDefault(x => x.IsPurchasable), MyHero.Inventory.GetFreeSlots(ItemSlot.MainSlot_1, ItemSlot.MainSlot_6).FirstOrDefault()))
+                {
+                    Console.WriteLine("11");
+                }
+                Sleeper5.Sleep(1000);
+            }
+
+
+
+
+
         }
     }
 }
